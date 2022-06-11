b57f05a4e7f420b89364e87aa8f4c5596e623812
Zheng Zeng <zhengzeng73@gmail.com>
2022-06-11 16:45:35 +0300

    fuc finished

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,41 @@
-import { shallowMount } from '@vue/test-utils'
+// Libraries
+import Vuetify from 'vuetify'
+
+// Components
 import HelloWorld from '@/components/HelloWorld.vue'
 
+// Utilities
+import { createLocalVue, mount } from '@vue/test-utils'
+
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const testMsg = 'nice'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { testMsg }
+  const localVue = createLocalVue()
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+
+  it('should emit an event when the action v-btn is clicked', () => {
+    const wrapper = mount(HelloWorld, {
+      localVue,
+      vuetify,
     })
-    expect(wrapper.text()).toMatch(testMsg)
+
+    const event = jest.fn()
+    const button = wrapper.find('.v-text-field')
+
+    // Here we bind a listener to the wrapper
+    // instance to catch our custom event
+    // https://vuejs.org/v2/api/#Instance-Methods-Events
+    wrapper.vm.$on('action-btn:clicked', event)
+
+    expect(event).toHaveBeenCalledTimes(0)
+
+    // Simulate a click on the button
+    button.trigger('click')
+
+    // Ensure that our mock event was called
+    expect(event).toHaveBeenCalledTimes(1)
   })
 })
